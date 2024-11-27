191a9487bf56b0c2b312af65827330db4fbff529
Gavo306ina <116042426+Gavo306ina@users.noreply.github.com>
2024-11-27 11:57:57 -0300

    Add files via upload

---
+++ leche-main/leche-main/chat.py
@@ -0,0 +1,43 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+# Conectar a MongoDB
+client = MongoClient('mongodb+srv://nose:nose@nose.87wxfml.mongodb.net/')
+db = client['nose']
+collection = db['leche']
+
+
+def litros(proveedor_name, litros_nuevos):
+    # Asegurarse de que 'litros' sea un array en el proveedor correspondiente
+    resultado = collection.update_one(
+        {"proveedors.name": proveedor_name},  # Buscar el proveedor por su nombre
+        {
+            "$setOnInsert": {
+                "proveedors.$.litros": []  # Inicializar 'litros' como un array vacío si no existe
+            }
+        },
+        upsert=True  # Crear el proveedor si no existe
+    )
+
+    # Ahora agregamos los nuevos litros
+    resultado = collection.update_one(
+        {"proveedors.name": proveedor_name},  # Buscar proveedor por nombre
+        {
+            "$push": {
+                "proveedors.$.litros": {  # Usar $push para agregar un nuevo elemento a 'litros'
+                    "litros_leche": litros_nuevos,
+                    "fecha_leche": datetime.now()  # Fecha actual en formato UTC
+                }
+            }
+        }
+    )
+
+    if resultado.modified_count > 0:
+        print(f"Se actualizó el proveedor {proveedor_name} con {litros_nuevos} litros de leche.")
+    elif resultado.upserted_id is not None:
+        print(f"Se creó el proveedor {proveedor_name} con {litros_nuevos} litros de leche.")
+    else:
+        print(f"No se encontró el proveedor {proveedor_name} o no se actualizó.")
+
+
+litros("yo", 24)
---
+++ leche-main/leche-main/db.py
@@ -0,0 +1,23 @@
+import os
+from pymongo.mongo_client import MongoClient
+
+MONGO_URI ='mongodb+srv://nose:nose@nose.87wxfml.mongodb.net/'
+
+MONGO_DB = os.environ.get("MONGO_DB") 
+
+class ConnectDB:
+    def __init__(self):
+       try:
+           self.conn = MongoClient(MONGO_URI)
+       except Exception as e:
+           print('Error al conectar a la base de datos: ', e)
+        
+    def get_conn(self):
+        return self.conn.get_database(MONGO_DB)
+    
+    def close_conn(self):
+        return self.conn.close()
+
+    def  ping(self):
+        return self.conn.admin.command('ping')
+    
